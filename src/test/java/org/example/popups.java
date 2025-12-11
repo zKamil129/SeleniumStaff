fe079b91ab80aad51632dee4b3893978bbd14e52
Kamil <kamillo0988@gmail.com>
2025-12-11 23:43:51 +0100

    popups exercise

--- src/test/java/org/example/popups.java
+++ src/test/java/org/example/popups.java
@@ -10,7 +10,7 @@ public class popups {
     static void main() {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
 
         System.out.println("DUPADUOPA");
         String text = "randomtext";

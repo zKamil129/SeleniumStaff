ef1dc802d1d976afd72030d3fdf3c8061eacb018
Kamil <kamillo0988@gmail.com>
2025-12-11 22:43:27 +0100

    pop up exercise

---
+++ src/test/java/org/example/popups.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class popups {
+    static void main() {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        WebDriver driver=new ChromeDriver();
+
+        System.out.println("DUPADUOPA");
+        String text = "randomtext";
+        driver.get("https://rahulshettyacademy.com/AutomationPractice");
+        driver.findElement(By.id("name")).sendKeys(text);
+        driver.findElement(By.id("alertbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+        driver.findElement(By.id("confirmbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().dismiss();
+
+
+    }
+}
